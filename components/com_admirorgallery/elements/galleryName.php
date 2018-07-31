1c846ef975f037dac3597e7a626c48f0f95c3327
sterlingrobot <sterlingrobot@gmail.com>
2018-07-31 11:02:28 -0600

    admirorgallery file

--- components/com_admirorgallery/elements/galleryName.php
+++ components/com_admirorgallery/elements/galleryName.php
@@ -1,42 +1,48 @@
 <?php
- 
+
+/* ------------------------------------------------------------------------
+  # com_admirorgallery - Admiror Gallery Component
+  # ------------------------------------------------------------------------
+  # author   Igor Kekeljevic & Nikola Vasiljevski
+  # copyright Copyright (C) 2014 admiror-design-studio.com. All Rights Reserved.
+  # @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+  # Websites: http://www.admiror-design-studio.com/joomla-extensions
+  # Technical Support:  Forum - http://www.vasiljevski.com/forum/index.php
+  # Version: 5.0.0
+  ------------------------------------------------------------------------- */
 // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
+class JFormFieldGalleryName extends JFormField {
 
-class JElementGalleryName extends JElement
-{
-   var   $_name = 'galleryName'; 
+    //var   $_name = 'galleryName'; 
+    public $type = 'galleryName';
 
+    protected function getInput() {
+        JHTML::_('behavior.modal');
 
-   function fetchElement($name, $value, &$node, $control_name)
-   {
+        // Initialize some field attributes.
+        $size = $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
+        $maxLength = $this->element['maxlength'] ? ' maxlength="' . (int) $this->element['maxlength'] . '"' : '';
+        $class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
+        $readonly = ((string) $this->element['readonly'] == 'true') ? ' readonly="readonly"' : '';
+        $disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
 
-JHTML::_('behavior.modal');
+        // Initialize JavaScript field attributes.
+        $onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
 
-      $size = ( $node->attributes('size') ? 'size="'.$node->attributes('size').'"' : '' );
-      $class = ( $node->attributes('class') ? 'class="'.$node->attributes('class').'"' : 'class="galleryName"' );
-
-      $value = htmlspecialchars(html_entity_decode($value, ENT_QUOTES), ENT_QUOTES);
-
-      $content = '';
-
-      $content.= '
-      <input
-	  name="'.$control_name.'['.$name.']" type="text"
-	  class="inputbox" id="'.$name.'"
-	  value="'.$value.'" size="20" />
-      ';
-      $link = 'index.php?option=com_admirorgallery&amp;view=galleryname&amp;tmpl=component&amp;e_name='.$name;
-      $content.= '
-	  <a href="'.$link.'" rel="{handler: \'iframe\', size: {x: 500, y: 400}}" class="modal" style="text-decoration:none;">
-		<button type="button" style="cursor:pointer; cursor:hand">'.JText::_('SELECT GALLERY').'</button>
+        $content = '<input type="text" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
+                . htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class . $size . $disabled . $readonly . $onchange . $maxLength . '/>';
+        $link = JRoute::_('index.php?option=com_admirorgallery&amp;view=galleryname&amp;tmpl=component&amp;e_name=' . $this->name);
+        $content.= '
+	  <a href="' . $link . '" rel="{handler: \'iframe\', size: {x: 500, y: 400}}" class="modal" style="text-decoration:none;">
+		<button type="button" class="btn">' . JText::_('AG_SELECT_GALLERY') . '</button>
 	  </a>
       ';
 
-      return $content;
-   }
-}
+        return $content;
+    }
 
+}
 
 ?>
