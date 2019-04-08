2a54d2b7f8ed102cc5d468ff69f1ef5bb0123d2c
sterlingrobot <sterlingrobot@gmail.com>
2019-04-08 00:42:56 -0600

    nav evt handling

--- templates/dancer2/js/nav.js
+++ templates/dancer2/js/nav.js
@@ -7,4 +7,20 @@ jQuery(document).ready(function($) {
 		return $(this).add('#navigation').toggleClass('open');
 	});
 
+	$('.nav-header').on('click', function(e) {
+		var $navItem = $(this);
+		e.stopPropagation();
+		return (
+			$('.nav-header').not($navItem).removeClass('open') &&
+			$navItem.toggleClass('open') &&
+			$navItem.hasClass('open') &&
+			$(document).off('click.nav').one('click.nav', function(e) {
+				if(e.target.href) { return true; }
+				return !$(e.target).is($navItem) &&
+					!$(e.target).closest($navItem).length &&
+					$navItem.removeClass('open');
+			})
+		);
+	});
+
 });
