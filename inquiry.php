a4f4abd86f6bd0208a2300443c6e1d6cf7d5e3cf
sterlingrobot <sterlingrobot@gmail.com>
2019-04-29 23:31:24 -0600

    inquiry form, endpoint

--- inquiry.php
+++ inquiry.php
@@ -1,16 +1,132 @@
 <?php
 
+define( '_JEXEC', 1 );
+define( 'DS', DIRECTORY_SEPARATOR );
+define( 'JPATH_BASE', $_SERVER['DOCUMENT_ROOT'] );
+
+require_once( JPATH_BASE . DS . 'includes' . DS . 'defines.php' );
+require_once( JPATH_BASE . DS . 'includes' . DS . 'framework.php' );
+require_once( JPATH_BASE . DS . 'libraries' . DS . 'joomla' . DS . 'platform.php' );
+
+$mainframe =& JFactory::getApplication('site');
+
 $mailer = JFactory::getMailer();
 $config = JFactory::getConfig();
 
-$sender = array(
-    $config->get( 'mailfrom' ),
-    $config->get( 'fromname' )
-);
+$mailer->setSender(array(
+	$config->get('mailfrom'),
+	$config->get('fromname')
+));
 
-$mailer->setSender($sender);
-
-$user = JFactory::getUser();
-$recipient = $user->email;
+$recipient = 'webmaster@villagedancetheatre.com';
+$subject = 'Registration Inquiry from villagedancetheatre.com';
 
 $mailer->addRecipient($recipient);
+
+// convert array to object
+$request = json_decode(json_encode($_REQUEST));
+
+$children = array();
+
+foreach ($_REQUEST as $key => $val) {
+	if(preg_match('/^inputChild(\d+)(.*?)$/i', $key, $matches)) {
+		if(!is_object($children[$matches[1]])) {
+			$children[$matches[1]] = new stdClass();
+		}
+		$children[$matches[1]]->{$matches[2]} = $val;
+	}
+}
+
+$body = '<p><strong>' . $request->inputName . '</strong> submitted an online inquiry about registration.</p>';
+
+$body .= strlen($request->inputNotes) ?
+					'<blockquote>' . $request->inputNotes . '</blockquote>'
+					: '';
+
+$body .= isset($request->inputClassVisit) ?
+					'<blockquote>I\'d like to arrange a class visit.</blockquote>'
+					: '';
+
+$body .= isset($request->inputPerfVisit) ?
+					'<blockquote>I\'d like to arrange seeing an upcoming performance.</blockquote>'
+					: '';
+
+$body .= '<ul><strong>Contact Info</strong><br>';
+$body .= '	<li>' . $request->inputName . '</li>';
+$body .= strlen($request->inputPhone) ?
+					'	<li>' . $request->inputPhone . '</li>'
+					: '';
+$body .= '	<li>' . $request->inputEmail . '</li>';
+$body .= '</ul>';
+
+$body .= '<strong>New Student Info</strong>';
+$body .= '<table>';
+$body .= '	<thead>';
+$body .= '		<tr>';
+
+foreach (array('Name', 'Age', 'Level') as $hdg) {
+	$body .= '			<th>' . $hdg . '</th>';
+}
+
+$body .= '		</tr>';
+$body .= '	</thead>';
+
+$body .= '<tbody>';
+
+foreach ($children as $key => $child) {
+	$body .= '<tr>';
+	$body .= '	<td>';
+	$body .= implode('</td><td>',
+		array(
+			$child->Name,
+			$child->Age,
+			$child->Level,
+		)
+	);
+	$body .= '	</td>';
+	$body .= '</tr>';
+}
+
+$body .= '</tbody>';
+$body .= '</table>';
+
+$mailer->isHtml(true);
+$mailer->Encoding = 'base64';
+
+$mailer->addReplyTo($request->inputEmail, $request->inputName);
+$mailer->setSubject($subject);
+$mailer->setBody($body);
+
+if ($mailer->Send()) {
+
+	$out = json_encode(array(
+		'status' => 'success',
+		'heading' => 'Mail sent successfully!',
+		'body' => 'Thank you for your inquiry!<br>We will be in touch very soon with more details.'
+	));
+} else {
+
+	$body = preg_replace('/<\/(tr|ul|li|p)>/', '%0D%0A', $body);
+	$body = preg_replace('/<(br|table)>/', '%0D%0A', $body);
+	$body = preg_replace('/<(strong|blockquote|ul)>/', '%0D%0A%0D%0A', $body);
+	$body = str_replace('</td>', '%09%09', $body);
+	$body = str_replace('</th>', '%09%09', $body);
+	$body = strip_tags($body);
+
+  $out = json_encode(array(
+		'status' => 'error',
+		'heading' => 'Sorry, there was an error sending the email.',
+		'body' => 'Please email us directly using <a href="mailto: '
+			. $recipient
+			. '?subject=' . $subject
+			. '&body=' . $body
+			. '">this link</a>'
+	));
+}
+
+header('Content-type: application/json; charset="UTF-8"');
+header('Content-Length: '.strlen($out));
+header('Connection: close');
+echo $out;
+flush();
+
